cbf85a850a8265e39ab3400aa4f7e7a83512ed6d
klarahirao <klara.hirao@elpassion.pl>
2017-09-08 16:46:40 +0200

    Store user

--- app/bot/message.rb
+++ app/bot/message.rb
@@ -3,7 +3,9 @@ include Facebook::Messenger
 Bot.on :message do |input|
   input.typing_on
 
-  api_ai_response = ApiAi::RequestSender.new(session_id: input.sender['id'], message: input.text).get
+  User.create(psid: input.sender['id'])
+
+  api_ai_response = ApiAi::RequestSender.new(session_id: input.sender['id'], message: input.text).call
   parsed_response = ApiAi::ResponseParser.new(api_ai_response).call
   output          = ApiAi::OutputGenerator.new(parsed_response).call
 
