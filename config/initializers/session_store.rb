73c96db55f03c72c139d34b937f533c62765a4a0
klarahirao <klara.hirao@elpassion.pl>
2017-10-11 11:08:24 +0200

    Rename application

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_wat_bot_session'
+Rails.application.config.session_store :cookie_store, key: '_ruby_bot_workshops_session'
